554d4d0d983e3eed90ca2cc5d1cbb141d430afe9
Yurijfedor <99280782+Yurijfedor@users.noreply.github.com>
2022-12-04 18:40:25 +0000

    Deploying to gh-pages from @ Yurijfedor/goit-react-hw-08-phonebook@11870d8172fdca54ab525cae2471f1125d26f589 🚀

---
+++ static/js/114.c21f3c97.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[114],{6114:function(e,t,n){n.r(t),n.d(t,{default:function(){return a}});var r=n(184);function a(){return(0,r.jsx)("div",{children:(0,r.jsx)("h1",{children:"Contacts manager welcome page "})})}}}]);
+//# sourceMappingURL=114.c21f3c97.chunk.js.map
